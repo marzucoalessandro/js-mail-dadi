31b2571196f999f6a2beb40830ce29b9697ad4ed
Alessandro <marzucoalessandro1@gmail.com>
2020-11-17 18:37:53 +0100

    prima parte esecizio senza messaggio

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,26 +4,30 @@
 //stampa un messaggio appropriato sull’esito del controllo.
 
 //1 creiamo una var con un prompt per chiedere il nome della persona
-var domanda = prompt("quale è il tuo nome?");
+var domanda = prompt("quale è la tua mail?");
 console.log(domanda);
 //controlliamo nella console il prompt
 
-//2 associamo una variabile a un email e ripetiamo l'operazione per 3 persone
-var alessandro = "alessandro@gmail.com";
-var luca = "luca@gmail.com";
-var matteo = "matteo@gmail.com";
-console.log(alessandro, luca, matteo);
-//controlliamo nella console  le tre variabili;
 
+//2 creiamo un array con una lista di email
+var listamail = ["alessandro@gmail.com", "luca@gmail.com", "matteo@gmail.com"];
+console.log(listamail);
 
-//3 creiamo un array con una lista di nomi
-var listanomi = ["alessandro", "luca", "matteo"];
-//verifichiamo qst nomi nella console
-
-//3 creiamo un if per vedere se il nome scritto è presente nell'array:
-if (domanda = listanomi[0, 1, 2]) {
-
+//3 creiamo un ciclo for per vedere se il nome scritto è presente nell'array:
+console.log(listamail[0]);
+console.log(listamail[1]);
+console.log(listamail[2]);
+var trovato = false;
+for (var i = 0; i < listamail.length; i++) {
+  if (listamail[i] == domanda) {
+    trovato = true
+  }
+  
 }
+console.log(trovato);
+
+
+
 
 // in caso positivo stampiamo un msg
 
