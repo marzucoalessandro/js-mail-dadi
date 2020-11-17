4f79ce5ce23cbd91968804f663eb1f3b431b1c1d
Alessandro <marzucoalessandro1@gmail.com>
2020-11-17 15:50:40 +0100

    inizio esercizio

---
+++ asset/js/main.js
@@ -0,0 +1,31 @@
+//Mail
+//Chiedi all’utente la sua email,
+//controlla che sia nella lista di chi può accedere,
+//stampa un messaggio appropriato sull’esito del controllo.
+
+//1 creiamo una var con un prompt per chiedere il nome della persona
+var domanda = prompt("quale è il tuo nome?");
+console.log(domanda);
+//controlliamo nella console il prompt
+
+//2 associamo una variabile a un email e ripetiamo l'operazione per 3 persone
+var alessandro = "alessandro@gmail.com";
+var luca = "luca@gmail.com";
+var matteo = "matteo@gmail.com";
+console.log(alessandro, luca, matteo);
+//controlliamo nella console  le tre variabili;
+
+
+//3 creiamo un array con una lista di nomi
+var listanomi = ["alessandro", "luca", "matteo"];
+//verifichiamo qst nomi nella console
+
+//3 creiamo un if per vedere se il nome scritto è presente nell'array:
+if (domanda = listanomi[0, 1, 2]) {
+
+}
+
+// in caso positivo stampiamo un msg
+
+//4 se non è presente nella ns lista allora
+// nn stampiamo il messaggio.
